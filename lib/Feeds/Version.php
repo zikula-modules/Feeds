c77f278af46a91e2ddb64a2c61c7a98a3ce7df62
Drak <drak@zikula.org>
2010-07-06 09:17:54 +0000

    Api changes.

---
+++ lib/Feeds/Version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Feeds
+ *
+ * @copyright (c) 2002, Zikula Development Team
+ * @link      http://code.zikula.org/feeds/
+ * @license   GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ */
+
+class Feeds_Version extends Zikula_Version
+{
+    public function getMetaData()
+    {
+        $meta = array();
+        $meta['displayname']    = __('Feeds', $dom);
+        $meta['description']    = __('The Feeds module provides a feed reader to your website.', $dom);
+        $meta['url']            = __(/*!module name that appears in URL*/'feeds', $dom);
+        $meta['version']        = '2.6';
+        $meta['contact']        = 'http://zikula.org/';
+        $meta['securityschema'] = array('Feeds::Item' => 'Feed item name::Feed item ID');
+        return $meta;
+    }
+}
