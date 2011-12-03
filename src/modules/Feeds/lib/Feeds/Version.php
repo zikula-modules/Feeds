2442e3cff7ebda36efde0d9d93fbc318461daa42
Drak <drak@zikula.org>
2011-12-03 09:17:42 +0545

    Set core requirement to 1.3.x

--- src/modules/Feeds/lib/Feeds/Version.php
+++ src/modules/Feeds/lib/Feeds/Version.php
@@ -14,7 +14,9 @@ class Feeds_Version extends Zikula_AbstractVersion
         $meta['displayname']    = __('Feeds');
         $meta['description']    = __('The Feeds module provides a feed reader to your website.');
         $meta['url']            = __(/*!module name that appears in URL*/'feeds');
-        $meta['version']        = '2.6.0';
+        $meta['version']        = '2.6.1';
+        $meta['core_min'] = '1.3.0'; // Fixed to 1.3.x range
+        $meta['core_max'] = '1.3.99'; // Fixed to 1.3.x range
         $meta['contact']        = 'http://zikula.org/';
         $meta['securityschema'] = array('Feeds::Item' => 'Feed item name::Feed item ID');
         return $meta;
